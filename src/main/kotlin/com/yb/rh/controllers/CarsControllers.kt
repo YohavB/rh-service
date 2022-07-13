4d4e71edfb7380b5358ced7a65ea23d2da4d6e37
YohavB <yohavbenoliel@gmail.com>
2022-07-13 11:43:16 +0300

    fix path and parse it with okhttp and Gson doing some order in all the files

--- src/main/kotlin/com/yb/rh/controllers/CarsControllers.kt
+++ src/main/kotlin/com/yb/rh/controllers/CarsControllers.kt
@@ -1,13 +1,7 @@
 package com.yb.rh.controllers
 
-import com.yb.rh.entities.*
-import com.yb.rh.repositorties.CarsRepository
-import com.yb.rh.repositorties.UsersCarsRepository
-import com.yb.rh.repositorties.UsersRepository
 import com.yb.rh.services.CarsService
-import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
-import org.springframework.web.server.ResponseStatusException
 
 @RestController
 @RequestMapping("/api/cars")
@@ -22,9 +16,13 @@ class CarsController(
     fun findByPlateNumber(@RequestParam(name = "plateNumber") plateNumber: String) =
         carsService.findByPlateNumber(plateNumber)
 
+    @GetMapping("/carinfo")
+    fun getCarInfo(@RequestParam(name = "plateNumber") plateNumber: String) =
+        carsService.getCarInfo(plateNumber)
+
     @PostMapping("/car")
     fun createOrUpdateCar(
-        @RequestBody carsDTO: CarsDTO,
+        @RequestParam(name = "plateNumber", required = true) plateNumber: String,
         @RequestParam(name = "userId", required = false) userId: Long?
-    ) = carsService.createOrUpdateCar(carsDTO, userId)
+    ) = carsService.createOrUpdateCar(plateNumber, userId)
 }
